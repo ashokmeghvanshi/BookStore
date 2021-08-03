5071c255fb227d8d7f37daf4c354b701c54e8925
Ashok Kumar Meghvanshi <ashokmeghvanshi04@gmail.com>
2021-08-03 15:10:52 +0530

    Added Jquery and Bootstrap To Application

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -8,9 +8,13 @@ namespace BookStore.Controllers
 {
     public class HomeController: Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "hello";
+            return View();
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
         }
     }
 }
